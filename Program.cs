22a1657a8d56bbd76ff4f25bb0e2c2e7649c5561
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-10-01 17:31:57 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MaximumProductSubarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums= {2,3,-2,4};
+
+            int result = MaxProduct(nums);
+            Console.WriteLine(result);
+
+        }
+
+        public static int MaxProduct(int[] nums) {
+            int result = nums[0];
+            int currentMin = nums[0];
+            int currentMax = nums[0];
+            for(int i = 1; i < nums.Length; i++)
+            {
+                if(nums[i] == 0)
+                {
+                    result = Math.Max(result, 0);
+                    currentMin = 1;
+                    currentMax = 1;
+                    continue;
+                }
+                
+                int[] options = new int[] {currentMax * nums[i], currentMin * nums[i], nums[i]};
+                Array.Sort(options);
+                currentMin = options[0];
+                currentMax = options[2];
+                result = Math.Max(result, currentMax);
+            }
+            return result;
+        }
+    }
+}
